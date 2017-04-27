9572e865251b87bb8b636061d3cc9602bbcf0067
rlim2 <rlim2@buffalo.edu>
2017-04-27 18:52:06 -0400

    Research and side bar incomplete

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 //   $('#mycarousel').carousel();
 // });
 $('#mycarousel').carousel();
+
+$('#sidebar').affix({
+  offset: {
+    top: 250
+  }
+})
+
+// $(window).ready(function() {
+//   var sidebarWidth = $('#sidebar').width();
+//
+// });
