7c97fc5097bbb2c0510045cbde72c0a53231dda2
rlim2 <rlim2@buffalo.edu>
2017-05-02 22:27:40 -0400

    Almost ready to launch! =)

--- js/main.js
+++ js/main.js
@@ -13,9 +13,10 @@ $(window).ready(function() {
   $('#hamburger').click(function() {
     if ($(this).hasClass('open')) {
       $(this).removeClass('open');
-      // $('')
+      $('#nav').removeClass('open');
     } else {
       $(this).addClass('open');
+      $('#nav').addClass('open');
     }
   });
 });
